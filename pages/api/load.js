9c73e07679f4fbfe437003173d3d150424604ab5
Juan Jose <juanobando.developer@gmail.com>
2022-11-23 10:46:47 -0500

    fixed #3

--- pages/api/load.js
+++ pages/api/load.js
@@ -2,6 +2,8 @@ import { getAllUsers } from '../../database/db';
 
 export default async function handler(req, res) {
     res.status(200).json({ name: 'John Doe' });
+    console.log(1);
     await getAllUsers();
+    console.log(2);
   }
   
